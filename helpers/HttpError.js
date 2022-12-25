9a17efd9731ac82972a7e130e9021146752e355b
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-12-25 20:41:16 +0200

    Add all tasks of HW 2

---
+++ helpers/HttpError.js
@@ -0,0 +1,12 @@
+const messages = {
+  400: "missing required name field",
+  404: "Not found",
+};
+
+const HttpError = (status, message = messages[status]) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
+
+module.exports = HttpError;
